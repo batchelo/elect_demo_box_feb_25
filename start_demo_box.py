a6371419b692e1d85013fd6a58f3195f67d143e2
Nim Batchelor <batchelo@elon.edu>
2025-02-07 16:23:46 -0500

    Add files via upload
    revisions of this code

--- start_demo_box.py
+++ start_demo_box.py
@@ -25,7 +25,7 @@ from lcd.lcd import CursorMode
 #for the accelerometer
 from adafruit_bus_device.i2c_device import I2CDevice 
 import adafruit_lis3dh
-
+import random
 
 #define some properties that will be used later
 
@@ -151,9 +151,7 @@ def play_lights(filename, count):
     # needed for persistance before next demo
     time.sleep(2)
 
-
 def play_display(filename, seconds):
-
     lcd.clear()
     # Start at the second line, fifth column (numbering from zero).
     lcd.set_cursor_pos(0, 4)
@@ -169,8 +167,7 @@ def play_display(filename, seconds):
     ####
     # needed for persistance before next demo
     time.sleep(seconds)
-
-        
+  
 def play_temp(filename, seconds):
     state = False
     decoder.file = open(path + filename, "rb")
@@ -194,6 +191,38 @@ def play_temp(filename, seconds):
         audio.play(decoder)
     # needed for persistance before next demo
     time.sleep(seconds)
+    
+def play_kk_joke(case_value):
+    if case_value == 1:
+        decoder.file = open(path + "drum.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+    if case_value == 2:
+        decoder.file = open(path + "donut.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+    if case_value == 3:
+        decoder.file = open(path + "icecream.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+    if case_value == 4:
+        decoder.file = open(path + "thankyou.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+    if case_value == 5:
+        decoder.file = open(path + "vlad.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+    if case_value == 6:
+        decoder.file = open(path + "welcome_forge.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
                 
 def play_tilt(filename, duration):
     decoder.file = open(path + filename, "rb")
@@ -230,26 +259,33 @@ def play_tilt(filename, duration):
 #  - 8G = 10-20 threshold
 #  - 16G = 5-10 threshold
     lis3dh.set_tap(2, 20)
-    for i in range(duration):
-        time.sleep(2)
-        if lis3dh.tapped:
-            print("Tapped!")
-            decoder.file = open(path + "drum.mp3", "rb")
-            audio.play(decoder)
-            while audio.playing:
-                pass
+    flag = 1
+#     for i in range(duration):
+#         time.sleep(2)
+    if lis3dh.tapped and flag == 1:
+        print("Tapped!")
+        decoder.file = open(path + "drum.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+        time.sleep(.5)
+        decoder.file = open(path + "again.mp3", "rb")
+        audio.play(decoder)
+        while audio.playing:
+            pass
+        flag = 2
+        time.sleep(.5)
+        print ("flag = ", flag)
+    if flag == 2:      #lis3dh.tapped &
+        for i in range (15):
+            if lis3dh.tapped:
+                case_value = random.randint(1, 6)
+                print ("case value = ", case_value)
+                play_kk_joke(case_value)
+                flag = 1
             time.sleep(.5)
-            decoder.file = open(path + "again.mp3", "rb")
-            audio.play(decoder)
-            time.sleep(.5)
-'''            for i in range(duration):
-                if lis3dh.tapped:
-                    print("Tapped second time!")
-                    decoder.file = open(path + "vlad.mp3", "rb")
-                    audio.play(decoder)
-                    while audio.playing:
-                        pass 
-''' 
+            print ("flag = ", flag)
+                
 def color_all(pixels, color):
     pl = len(pixels)
     print('pixels length', pl)
@@ -365,18 +401,7 @@ def demo_set1():
         play_distance("sense_distance.mp3", 30)
         play_UM("UM_respond.mp3")
         play_hope("hope.mp3")
-'''
-def demo_set2():    
-#go into POST-INTRO state: (1) display "Forge" text, (2) blink lights in a pattern, (3) monitor UM switch
-    while True:
-        play_display(None, 7)
-        #play_display("display.mp3", 7)
-        play_lights(None, 20)
-        #play_lights("lights.mp3", 20)
-        play_UM(None)
-        #play_UM("UM_respond.mp3")
-'''
-        
+       
 def demo_set2():    
     #go into POST-INTRO state: (1) display "Forge" text, (2) blink lights in a pattern, (3) monitor UM switch
     while True:
@@ -403,7 +428,6 @@ def demo_set2():
                 pixels.show()
                 time.sleep(0.5)
             #rainbow_cycle(pixels, 0.001)  # rainbow cycle with 1ms delay per step
-
             pixels.fill((0, 0, 0))
             pixels.show()
     # needed for persistance before next demo
@@ -415,78 +439,11 @@ def demo_set2():
             angle = 85
             my_servo.angle = angle
             time.sleep(1)
-    for i in range(duration):
-        time.sleep(2)
-        if lis3dh.tapped:            
-            print("Tapped!")
-            decoder.file = open(path + "drum.mp3", "rb")
-            audio.play(decoder)
-            while audio.playing:
-                pass
+#        for i in range(20):
+        time.sleep(.5)
+        for i in range (10):
+            if lis3dh.tapped:
+                case_value = random.randint(1, 6)
+                print ("case value = ", case_value)
+                play_kk_joke(case_value)
             time.sleep(.5)
-            decoder.file = open(path + "again.mp3", "rb")
-            audio.play(decoder)
-            time.sleep(.5)            
-'''
-#Ideally, I would have it such that:
-#    (a) it would constantly be sensing whether or not it had been tapped twice
-#    (b) it would cycle between (or randomly select) one of six things to say in response to a double-tap
-#        (i) ask about whether it has become a drum--drum.mp3
-#        (ii) tell the donut kk joke -- dounut.mp3
-#        (iii) tell the icecream kk joke -- icecream.mp3
-#        (iv) tell the thank you kk joke -- thankyou.mp3
-#        (v) tell the vlad kk joke -- vlad.mp3
-#        (vi) tell the welcome kk joke --welcome_forge.mp3
-
-
-    lis3dh.range = adafruit_lis3dh.RANGE_2_G
-# Set tap detection to double taps.  The first parameter is a value:
-#  - 0 = Disable tap detection.
-#  - 1 = Detect single taps.
-#  - 2 = Detect double taps.
-# The second parameter is the threshold and a higher value means less sensitive
-# tap detection.  Note the threshold should be set based on the range above:
-#  - 2G = 40-80 threshold
-#  - 4G = 20-40 threshold
-#  - 8G = 10-20 threshold
-#  - 16G = 5-10 threshold
-    lis3dh.set_tap(2, 20)
-    import random
-    case_value = random.randint(1, 6)
-    print ("case value = ", case_value)
-    for i in range(duration):
-        time.sleep(2)
-        if lis3dh.tapped:
-            print ("tapped!")
-            if case_value == 1:
-                decoder.file = open(path + "drum.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-            if case_value == 2:
-                decoder.file = open(path + "donut.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-            if case_value == 3:
-                decoder.file = open(path + "icecream.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-            if case_value == 4:
-                decoder.file = open(path + "thankyou.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-            if case_value == 5:
-                decoder.file = open(path + "vlad.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-            if case_value == 6:
-                decoder.file = open(path + "welcome_forge.mp3", "rb")
-                audio.play(decoder)
-                while audio.playing:
-                    pass
-'''
-
