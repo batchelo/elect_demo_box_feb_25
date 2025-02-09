be49e11cba12604249edb4f14ee0450a80decccd
Nim Batchelor <batchelo@elon.edu>
2025-02-09 01:26:09 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import time
 import board
 import digitalio
 import start_demo_box as sdb
+#for the Elon box, the line above is "start_demo_box_Elon as sdb"
+#also, in the start_demo_box code, the hex address for the ultrasound device is 0x23 vs 0x27
 
 mode_pin = digitalio.DigitalInOut(board.GP2)
 
